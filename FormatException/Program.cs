c5f6245858f0a1a0ed2bc8a66fb1efb5dad974f8
Franklin J Lee <fjlee93@outlook.com>
2024-04-17 05:04:54 -0400

    added FormatException example

---
+++ FormatException/Program.cs
@@ -0,0 +1,10 @@
+﻿string myStringVar = "hello123";
+try
+{
+    int numStringVar = int.Parse(myStringVar);
+    Console.WriteLine($"Huzzah, the conversion was a success, here ya go: {numStringVar}");
+}
+catch (FormatException e)
+{
+    Console.WriteLine($"Conversion failed: {e.Message}");
+}
