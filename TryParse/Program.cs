6265c7bac3e1ca3ccd9fa1a145abea535436296f
Franklin J Lee <fjlee93@outlook.com>
2024-04-17 05:06:20 -0400

    added TryParse example

---
+++ TryParse/Program.cs
@@ -0,0 +1,10 @@
+﻿string stringVar1 = "999";
+if (int.TryParse(stringVar1, out int stringVar2))
+{
+    Console.WriteLine("Huzzah");
+    Console.WriteLine(stringVar2);
+}
+else
+{
+    Console.WriteLine("booooo....conversion failure");
+}
